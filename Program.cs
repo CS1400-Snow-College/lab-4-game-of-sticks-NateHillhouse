eacba954e57a1cac98798978b3c79a6f5dee41ca
mizterwaffles <nhillhouse04@gmail.com>
2025-09-23 09:04:25 -0600

    Finished processes, added extra credit steps

--- Program.cs
+++ Program.cs
@@ -8,25 +8,46 @@ Player 1, how many sticks would you like to take? 3
 Sticks left: 17
 Player 2, how many sticks would you like to take? 3
 */
+using System.Runtime.CompilerServices;
 
-Console.WriteLine(@"Players will take turns removing between 1 and 3 of the remaining sticks.  
-The player that removes the last stick loses.");
-
-int sticks = 20, player = 1, playersticks;
-bool success;
-while (sticks > 0)
+int sticks = 20, player = 1, playersticks = 0;
+bool success = false;
+while (sticks >= 0)
 {
-    Console.WriteLine($"Player {player}, how many sticks will you take? ");
-    do success = Int32.TryParse(Console.ReadLine(), out playersticks);
-    while (success == false);
-    sticks -= playersticks; 
-    Console.WriteLine($"There are {sticks} left.");
-
-    if (sticks == 0)
+    do
     {
-        Console.WriteLine($"Player {player} won!");
-        break;
+        Console.Clear();
+        Console.WriteLine("----------------------------------\n- Welcome to the game of sticks! -\n----------------------------------");
+        Console.WriteLine();
+        Console.WriteLine("Players will take turns removing between 1 and 3 of the remaining sticks.\nThe player that removes the last stick loses.");
+        Console.WriteLine();
+
+        for (int i = 1; i <= sticks; i++) Console.Write("|");
+        Console.WriteLine("\n");
+        Console.WriteLine($"There are {sticks} sticks left.");
+        if (sticks > 0)
+        {
+            Console.Write($"Player {player}, how many sticks will you take? ");
+            success = Int32.TryParse(Console.ReadLine(), out playersticks);
+            if (playersticks > 3 || playersticks < 1)
+            {
+                Console.WriteLine("Please pick a number between 1 and 3");
+                success = false;
+            }
+            if (success && sticks - playersticks < 0)
+            {
+                Console.WriteLine($"Sorry, there are not {playersticks} left.");
+                success = false;
+            }
+        }        
     }
-    if (player == 1 && sticks > 0) player = 2;
-        else if (player == 2 && sticks > 0) player = 1;
+    while (success == false);
+    sticks -= playersticks;
+
+    Console.WriteLine();
+    if (player == 1) player = 2;
+    else if (player == 2) player = 1;
 } 
+Console.WriteLine($@"/---------------\
+| Player {player} won! |
+\---------------/");
--- obj/Debug/net9.0/lab-4-game-of-sticks-NateHillhouse.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab-4-game-of-sticks-NateHillhouse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-4-game-of-sticks-NateHillhouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04962bb7ba571c4c22f69a3143195093020fc407")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1bfcfefd73ec384cc02869324461e1f8061a49f")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-4-game-of-sticks-NateHillhouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-4-game-of-sticks-NateHillhouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
